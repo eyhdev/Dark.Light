94e09d70022ef91a70490b0dfc36d6de66d54fe4
eyhdev <alaqsa335@gmail.com>
2023-12-25 13:59:57 +0300

    Initial Commit

---
+++ Dark.Light/Dark_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dark_LightApp.swift
+//  Dark.Light
+//
+//  Created by eyh.mac on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dark_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

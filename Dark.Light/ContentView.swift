b23bda70f7100bd8f4308fb248b107de077226a3
eyhdev <alaqsa335@gmail.com>
2023-12-25 14:27:09 +0300

    Dark Light

--- Dark.Light/ContentView.swift
+++ Dark.Light/ContentView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isDarkMode: Bool = false
+    @State private var circleScale: CGFloat = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                isDarkMode ? Color.black : Color.white
+                
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 1)) {
+                        self.isDarkMode.toggle()
+                    }
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        self.circleScale = 5
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            self.circleScale = 0
+                        }
+                    }
+                })
+                 {
+                   Image(systemName: isDarkMode ? "moon.fill" : "sun.max.fill")
+                       .font(.largeTitle)
+                       .foregroundColor(isDarkMode ? .white : .yellow)
+                       .frame(width: 100, height: 100)
+                       .background(Circle()
+                           .fill(isDarkMode ? .white : .black)
+                           .scaleEffect(circleScale)
+                           .opacity(circleScale == 5 ? 0 : 1)
+                                   
+                       )
+               }
+                 .shadow(radius: 10)
+            }
+                .edgesIgnoringSafeArea(.all)
+                .onChange(of: isDarkMode){ newValue in
+                    UIApplication.shared.windows.first?.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
+                    
+                }
+                .navigationTitle("Light - Dark")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+    
 }
